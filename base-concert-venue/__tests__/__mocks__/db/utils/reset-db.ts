7380986b7c8804c05b25682a4243b2f84130cc40
Maarij <maarij.anwar@gmail.com>
2024-02-11 09:07:49 -0600

    test up a test json database

---
+++ base-concert-venue/__tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,24 @@
+import { readFakeData } from "../../fakeData";
+
+import { filenames, writeJSONToFile } from "@/lib/db/db-utils";
+
+export const resetDB = async () => {
+  // failsafe against resetting production db!
+  const safeToReset = process.env.NODE_ENV === "test";
+  if (!safeToReset) {
+    console.log(
+      "WARNING: database reset unavailable outside of test environment!"
+    );
+    return;
+  }
+
+  const { fakeShows, fakeBands, fakeUsers, fakeReservations } = await readFakeData();
+
+  // overwrite data in files
+  await Promise.all([
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+    writeJSONToFile(filenames.users, fakeUsers),
+  ]);
+};
